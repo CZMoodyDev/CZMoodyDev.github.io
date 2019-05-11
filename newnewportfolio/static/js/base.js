f9ecbe213d897d09e43c38903bd4eabccf783706
Curtis Zim Moody <czmoodydev@gmail.com>
2019-05-11 00:04:11 -0700

    Rotating cards added

---
+++ newnewportfolio/static/js/base.js
@@ -0,0 +1,17 @@
+$(".card-flip").toggleClass("flip");
+
+$( ".card-flip" ).click(function(e) {
+    if (e.target.className.indexOf('fa-') === -1 || e.target.className.indexOf('fa-sync') !== -1) {
+        if (  $( this ).css( "transform" ) == 'none' ){
+            $(this).css("transform","rotateY(180deg)");
+        } else {
+            $(this).css("transform","" );
+        }
+    }
+});
+
+$("#portfolio .fa-external-link-alt, #portfolio .fa-steam, #portfolio .fa-youtube").hover(function() {
+    $(this).prev().css("color", "#80556f");
+}, function(){
+    $(this).prev().css("color", "#ad7a99");
+});
