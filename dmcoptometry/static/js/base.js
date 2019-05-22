3b6ee1f2cea7bc317a4dc024f4728c7309d8f950
Curtis Zim Moody <czmoodydev@gmail.com>
2019-05-21 23:06:49 -0700

    Updates to dmcoptometry example site

--- dmcoptometry/static/js/base.js
+++ dmcoptometry/static/js/base.js
@@ -2,6 +2,38 @@ $('.navbar-nav>li>a').on('click', function(){
     $('.navbar-collapse').collapse('hide');
 });
 
+function turnOnLethbridge() {
+    $("#lethbridgeMap").show();
+    $("#lethbridgeControl").addClass("active-control");
+    $("#medicineHatMap").hide();
+    $("#medicineHatControl").removeClass("active-control");
+}
+
+function turnOnMedicineHat() {
+    $("#medicineHatMap").show();
+    $("#medicineHatControl").addClass("active-control");
+    $("#lethbridgeMap").hide();
+    $("#lethbridgeControl").removeClass("active-control");
+}
+
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
+
 /* Fixed position with footer by Lionel Paulus https://codepen.io/lionelpaulus/pen/YGXwxr */
 var socialFloat = document.querySelector('#scheduleMobileBlock');
 var footer = document.querySelector('#footer');
